ce54f6b84104b563b791c28b7b681f8fda5b87f0
LaayekIbneHabib <laayekibnehabib3003@gmail.com>
2024-04-26 19:11:38 +0530

    I added some authentication related code to the codebase.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+// default
+import React from "react";
 // internal
+import { authConfig, loginIsRequiredServer } from "../../lib/auth";
 import { SidebarContent } from "../components/composite/nav/sidebarContent";
 import { MobileNav } from "../components/composite/nav/mobileNav";
 // external
@@ -12,38 +15,44 @@ import {
   useDisclosure,
   Container,
 } from "@chakra-ui/react";
+import { getServerSession } from "next-auth";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Layout = ({ children }: LayoutProps) => {
+export default async function Layout({ children }: LayoutProps) {
+  await loginIsRequiredServer();
+  const session = await getServerSession(authConfig);
+
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
-      <SidebarContent
-        onClose={onClose}
-        display={{ base: "none", md: "block" }}
-      />
-      <Drawer
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        returnFocusOnClose={false}
-        onOverlayClick={onClose}
-        size="full"
-      >
-        <DrawerContent>
-          <SidebarContent onClose={onClose} />
-        </DrawerContent>
-      </Drawer>
-      <MobileNav onOpen={onOpen} />
-      <Box ml={{ base: 0, md: 60 }} py="4" overflowX="hidden">
-        <Container maxW="container.xl">{children}</Container>
-      </Box>
-    </Box>
+    <React.Fragment>
+      {session && (
+        <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+          <SidebarContent
+            onClose={onClose}
+            display={{ base: "none", md: "block" }}
+          />
+          <Drawer
+            isOpen={isOpen}
+            placement="left"
+            onClose={onClose}
+            returnFocusOnClose={false}
+            onOverlayClick={onClose}
+            size="full"
+          >
+            <DrawerContent>
+              <SidebarContent onClose={onClose} />
+            </DrawerContent>
+          </Drawer>
+          <MobileNav onOpen={onOpen} />
+          <Box ml={{ base: 0, md: 60 }} py="4" overflowX="hidden">
+            <Container maxW="container.xl">{children}</Container>
+          </Box>
+        </Box>
+      )}
+    </React.Fragment>
   );
-};
-
-export default Layout;
+}
