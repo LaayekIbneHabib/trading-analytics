a7e7669e8fb0b568193016e63cac689799bea14c
LaayekIbneHabib <laayekibnehabib3003@gmail.com>
2024-05-14 01:12:13 +0530

    I implemented route protection.

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,15 @@
-export { auth as middleware } from './auth';
+import { NextResponse } from 'next/server';
+import { auth } from '@/auth';
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
+
+export default auth(req => {
+  const reqUrl = new URL(req.url);
+  if (!req.auth && reqUrl?.pathname !== '/login') {
+    return NextResponse.redirect(
+      new URL(`/login?callbackUrl=${encodeURIComponent(reqUrl?.pathname)}`, req.url),
+    );
+  }
+});
